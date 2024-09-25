4d21fb4e9721d60efec56586d6e62af8912f8885
lmq94 <quiroga9409@gmail.com>
2024-09-25 15:40:48 -0300

    Creo codigo de prueba para crear entidades y persistirlas en la db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import helper.EntityManagerJPA;
+import model.Carrera;
+import model.Estudiante;
 import repository.CarreraRepositoryImpl;
 import repository.EstudianteRepositoryImpl;
 import repository.RepositoryFactory;
@@ -16,6 +18,13 @@ public class App
         RepositoryFactory rf = new RepositoryFactory();
         EstudianteRepositoryImpl estudianteRepo= rf.getEstudianteRepository(em);
         CarreraRepositoryImpl carreraRepo= rf.getCarreraRepository(em);
+
+        //Estudiante e1 = new Estudiante("Lucas", "Duana", 24, "Hombre", "42630319", "Rauch","Numero de libreta del Duana", false);
+        //Carrera c1 = new Carrera("Tudai", 2);
+
+        //estudianteRepo.save(e1);
+        //carreraRepo.save(c1);
+        estudianteRepo.matricularEstudianteEnCarrera(1L,1L);
 
 
 
