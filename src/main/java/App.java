3da58d0f0bc82f7137c73d86b35861d5a418a5b4
lmq94 <quiroga9409@gmail.com>
2024-10-01 22:10:48 -0300

    Ultimos cambios

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,42 +15,58 @@ import java.util.List;
 public class App 
 {
     public static void main( String[] args ) throws Exception {
+
+
         EntityManager em = EntityManagerJPA.getEntityManager();
 
         RepositoryFactory rf = new RepositoryFactory();
         EstudianteRepositoryImpl estudianteRepo= rf.getEstudianteRepository(em);
         CarreraRepositoryImpl carreraRepo= rf.getCarreraRepository(em);
+
+
+
         LoadCsv cargador = new LoadCsv(rf);
+
+        //EJECUTAR SOLO UNA VEZ
         /*cargador.leerEstudiantes();
         cargador.leerCarreras();
-        cargador.leerEstudiantesCarreras();*/
-
-        /*
-        * a) dar de alta un estudiante
-b) matricular un estudiante en una carrera
+        cargador.leerEstudiantesCarreras();**/
 
 
+        //2)
+
+
+         //a) dar de alta un estudiante
+        //estudianteRepo.save(new Estudiante()); //por defecto se crea uno vacio
+
+
+        //b) matricular un estudiante en una carrera
+
+        //estudianteRepo.matricularEstudianteEnCarrera ----> METODO
+        // se debe ingresar como parametro dni de estudiante, id carrera, año de inscripcion, año de graduacion, antiguedad
+        // Y OBVIAMENTE ADENTRO DE PARENTESIS POR QUE ES UN METODO
 
 
 
-        * */
-
-        //d) recuperar un estudiante, en base a su número de libreta universitaria.
+        //d) RECUPERAR UN ESTUDIANTE, EN BASE A SU NUMERO DE LIBRETA UNIVERSITARIA.
         //System.out.println(estudianteRepo.obtenerEstudianteLibreta("61607"));
 
-        //e) recuperar todos los estudiantes, en base a su género.
+        //e) RECUPERAR TODOS LOS ESTUDIANTES EN BASE A SU GENERO
         //System.out.println(estudianteRepo.obtenerEstudiantesPorGenero("Polygender"));
 
-        //c) recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple.
+        //c) RECUPERAR TODOS LOS ESTUDIANTES , Y ESPECIFICAR ALGUN CRITERIO DE ORDENAMIENTO SIMPLE.
         //System.out.println(estudianteRepo.obtenerEstudiantesOrdenados("apellido"));
 
-        //f) recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.
+        //f) RECUPERAR LA CARRERAS CON ESTUDIANTES INSCRIPTOS, Y ORDERNAR POR CANTIDAD.
         //System.out.println(carreraRepo.obtenerCarrerasConEstudiantesInscriptos());
 
-        //g) recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia.
-        System.out.println(estudianteRepo.obtenerEstudiantesPorCarreraYCiudad(15L,"Jiaoyuan"));
+        //g) RECUPERAR A TODOS LOS ESTUDIANTES DE UNA DETERMINADA CARRERA, FILTRADOS POR CIUDAD DE RESIDENCIA.
+        //System.out.println(estudianteRepo.obtenerEstudiantesPorCarreraYCiudad(15L,"Jiaoyuan"));
 
+        //3) reporte inscriptos y graduados de las carreras ordenados cronologicamente por año
 
-
+        /*for(ReporteDTO r: carreraRepo.generarReporteCarreras()) {
+            System.out.println(r);
+        }**/
     }
 }
--- src/main/java/repository/CarreraRepositoryImpl.java
+++ src/main/java/repository/CarreraRepositoryImpl.java
@@ -59,22 +59,23 @@ public class CarreraRepositoryImpl extends RepositoryImpl<Carrera,Long> implemen
 
     public List<ReporteDTO> generarReporteCarreras() {
         TypedQuery<Object[]> query = em.createQuery(
-                "SELECT c.carrera, YEAR(ec.inscripcion), " +
-                        "SUM(CASE WHEN ec.graduacion IS NOT NULL THEN 1 ELSE 0 END) AS graduados, " +
-                        "COUNT(ec) AS inscriptos " +
+                "SELECT c.carrera, ec.inscripcion AS anio, " +
+                        "SUM(CASE WHEN ec.graduacion = 0 THEN 1 ELSE 0 END) AS graduados, " + // Graduados que tienen graduacion en 0
+                        "COUNT(CASE WHEN ec.graduacion > 0 THEN 1 END) AS inscriptos " + // Inscritos que no están graduados
                         "FROM EstudianteCarrera ec " +
                         "JOIN ec.carrera c " +
-                        "GROUP BY c.carrera, YEAR(ec.inscripcion) " +
-                        "ORDER BY c.carrera ASC, YEAR(ec.inscripcion) ASC",
+                        "WHERE ec.inscripcion IS NOT NULL " +
+                        "GROUP BY c.carrera, ec.inscripcion " +
+                        "HAVING COUNT(ec) > 0 " +
+                        "ORDER BY ec.inscripcion ASC, c.carrera ASC",
                 Object[].class
         );
-
         List<Object[]> resultados = query.getResultList();
         List<ReporteDTO> reporte = new ArrayList<>();
 
         for (Object[] fila : resultados) {
             String nombreCarrera = (String) fila[0];
-            int anio = (int) fila[1];
+            Integer anio = (Integer) fila[1]; // Obtener el año directamente
             long graduados = (long) fila[2];
             long inscriptos = (long) fila[3];
 
